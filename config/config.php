d53890c83e1a9129b834fd31fa05fecd842760d0
iyaki <ivanyaki96@gmail.com>
2023-09-26 19:41:24 -0300

    Borrado codigo innecesario

--- config/config.php
+++ config/config.php
@@ -38,9 +38,6 @@ $aggregator = new ConfigAggregator([
         : fn (): array => []
     ),
 
-    // Default App module config
-    App\ConfigProvider::class,
-
     // Load application config in a pre-defined order in such a way that local settings
     // overwrite global settings. (Loaded as first to last):
     //   - `global.php`
