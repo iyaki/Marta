6307bf7ebb156157d5722f0f35364e20183d4e12
iyaki <ivanyaki96@gmail.com>
2023-10-13 20:39:38 -0300

    Configuraciones doctrine migrations

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -14,6 +14,21 @@ return [
         ],
         'entitiesPaths' => [
             __DIR__ . '/../../app/Cuentas',
-        ]
+        ],
+        'migrations' => [
+            'table_storage' => [
+                'table_name' => 'doctrine_migration_versions',
+                'version_column_name' => 'version',
+                'version_column_length' => 191,
+                'executed_at_column_name' => 'executed_at',
+                'execution_time_column_name' => 'execution_time',
+            ],
+            'migrations_paths' => [
+                'App\Migrations' => __DIR__ . '/../../database/migrations',
+            ],
+            'all_or_nothing' => true,
+            'transactional' => true,
+            'check_database_platform' => true,
+        ],
     ],
 ];
