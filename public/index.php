170fa9ed6bf18a312184743283a18a7c91f0ac3c
iyaki <ivanyaki96@gmail.com>
2023-10-21 16:21:21 -0300

    Ajustes psalm

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Delegate static file requests back to the PHP built-in webserver
-if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
+if (PHP_SAPI === 'cli-server' && ($_SERVER['SCRIPT_FILENAME'] ?? null) !== __FILE__) {
     return false;
 }
 
@@ -14,17 +14,15 @@ require 'vendor/autoload.php';
  * Self-called anonymous function that creates its own scope and keeps the global namespace clean.
  */
 (function () {
-    /** @var \Psr\Container\ContainerInterface $container */
     $container = require 'config/container.php';
 
-    /** @var \Mezzio\Application $app */
     $app = $container->get(\Mezzio\Application::class);
-    $factory = $container->get(\Mezzio\MiddlewareFactory::class);
+    // $factory = $container->get(\Mezzio\MiddlewareFactory::class);
 
     // Execute programmatic/declarative middleware pipeline and routing
     // configuration statements
-    (require 'config/pipeline.php')($app, $factory, $container);
-    (require 'config/routes.php')($app, $factory, $container);
+    (require 'config/pipeline.php')($app, /* $factory, $container */);
+    (require 'config/routes.php')($app, /* $factory, $container */);
 
     $app->run();
 })();
