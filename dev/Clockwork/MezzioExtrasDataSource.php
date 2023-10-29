a2eafd05b88278d8f3e8f13ea02bd2e36765b534
iyaki <ivanyaki96@gmail.com>
2023-10-29 19:22:50 -0300

    Mejoras logging clockwork

--- dev/Clockwork/MezzioExtrasDataSource.php
+++ dev/Clockwork/MezzioExtrasDataSource.php
@@ -6,14 +6,18 @@ namespace MartaDev\Clockwork;
 
 use Clockwork\DataSource\DataSource;
 use Clockwork\Request\Request;
+use Mezzio\Application;
 use Mezzio\Helper\UrlHelperInterface;
+use Mezzio\Middleware\LazyLoadingMiddleware;
 use Mezzio\Router\RouteResult;
 use Psr\Http\Message\ServerRequestInterface;
+use rectObj;
 
 final class MezzioExtrasDataSource extends DataSource
 {
     public function __construct(
-        private readonly ServerRequestInterface $psrRequest
+        private readonly ServerRequestInterface $psrRequest,
+        private readonly Application $app
     )
     {}
 
@@ -22,8 +26,32 @@ final class MezzioExtrasDataSource extends DataSource
         /** @var RouteResult $routeResult */
         $routeResult = $this->psrRequest->getAttribute(RouteResult::class);
         $matchedRoute = $routeResult->getMatchedRoute();
-        $handler = $matchedRoute->getMiddleware()->middlewareName;
+        $middleware = $matchedRoute->getMiddleware();
 
-        $request->controller = $handler;
+        if ($middleware instanceof LazyLoadingMiddleware) {
+            $request->controller = $middleware->middlewareName;
+
+        }
+
+        $routes = $this->app->getRoutes();
+        foreach ($routes as $route) {
+            foreach ($route->getAllowedMethods() as $method) {
+                $middleware = $route->getMiddleware();
+
+                $action = '';
+
+                if (\property_exists($middleware, 'middlewareName')) {
+                    $action = $middleware->middlewareName;
+                }
+
+                $request->addRoute(
+                    $method,
+                    $route->getPath(),
+                    $action,
+                    ['name' => $route->getName()]
+                );
+            }
+        }
+
     }
 }
