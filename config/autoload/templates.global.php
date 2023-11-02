8f5716969c0225757a2bbe824a67f6bf1c81ac04
iyaki <ivanyaki96@gmail.com>
2023-11-02 15:14:57 -0300

    Uso basico de templates + htmx boost

--- config/autoload/templates.global.php
+++ config/autoload/templates.global.php
@@ -2,13 +2,21 @@
 
 declare(strict_types=1);
 
+use League\Plates\Extension\Asset;
+use League\Plates\Extension\URI;
+
 return [
-    'templates'  => [
+    'templates' => [
         'paths' => [
-            // 'app'    => [__DIR__ . '/../../templates/app'],
-            'error'  => [__DIR__ . '/../../app/Presentation/Common/templates/error'],
+            // 'app' => [__DIR__ . '/../../templates/app'],
+            'error' => [__DIR__ . '/../../app/Presentation/Common/templates/error'],
             'layout' => [__DIR__ . '/../../app/Presentation/Common/templates/layout'],
+            'components' => [__DIR__ . '/../../app/Presentation/Common/templates/components'],
             'cuentas' => [__DIR__ . '/../../app/Presentation/Cuentas/templates'],
+        ],
+        'extensions' => [
+            new Asset(__DIR__ . '/../../public/'),
+            // new URI($_SERVER['PATH_INFO']),
         ],
     ],
 ];
